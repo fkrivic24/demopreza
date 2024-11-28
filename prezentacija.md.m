0a1ee014c9725e277d2b39a5589e9ef1779e5706
fkrivic24 <fkrivic24@unizd.hr>
2024-11-28 13:20:07 +0100

    Add files via upload

---
+++ prezentacija.md.m
@@ -0,0 +1,125 @@
+# Grammarly - Alat za Pisanje Bez Pogrešaka
+
+**Ime i prezime:** Filip Krivić
+**Studijski program:** Stručni prijediplomski studij Informacijske tehnologije  
+**Predmet:** Akademsko i tehničko pisanje  
+**Datum izrade:** 25.11.2024.
+
+---
+
+# Uvod
+
+**Što je Grammarly?**  
+Grammarly je alat koji provjerava pravopis, gramatiku i stil pisanja, omogućujući korisnicima stvaranje jasnih i profesionalnih tekstova.
+
+**Zašto je važan za IT stručnjake?**  
+- Pomaže u pisanju tehničke dokumentacije.  
+- Poboljšava formalnu komunikaciju poput e-mailova i izvještaja.  
+- Smanjuje jezične pogreške u pisanju.
+
+---
+
+# Osnovne funkcionalnosti
+
+**Što Grammarly nudi?**  
+- Provjera pravopisa i gramatike u realnom vremenu.  
+- Preporuke za poboljšanje stila.  
+- Personalizirane analize tona pisanja.  
+
+*Screenshot:*  
+![Primjer funkcionalnosti](path/to/screenshot1.png)
+
+---
+
+# Napredne mogućnosti
+
+**Dodatne funkcionalnosti (Premium verzija):**  
+- Detekcija plagijata.  
+- Preporuke za poboljšanje vokabulara.  
+- Specifične postavke za različite vrste pisanja (akademsko, poslovno).
+
+---
+
+# Praktični primjeri
+
+**Kako Grammarly funkcionira?**  
+1. Unesite tekst u sučelje ili integrirani alat.  
+2. Alat označava pravopisne i gramatičke pogreške.  
+3. Klikom na prijedlog ispravite pogrešku.  
+
+*Screenshot:*  
+![Primjer ispravljanja teksta](path/to/screenshot2.png)
+
+---
+
+# Upute za instalaciju
+
+**Kako instalirati Grammarly?**  
+1. Posjetite [Grammarly web stranicu](https://www.grammarly.com).  
+2. Registrirajte se besplatno.  
+3. Preuzmite dodatak za preglednik ili instalirajte desktop aplikaciju.
+
+---
+
+# Kako započeti rad
+
+**Prvi koraci u korištenju:**  
+- Nakon instalacije, otvorite alat u pregledniku ili aplikaciji.  
+- Kopirajte ili napišite tekst koji želite provjeriti.  
+- Pratite prijedloge za ispravke.
+
+*Screenshot:*  
+![Grammarly u radu](path/to/screenshot3.png)
+
+---
+
+# Prednosti Grammarlyja
+
+**Što Grammarly čini posebnim?**  
+- Jednostavno i intuitivno korisničko sučelje.  
+- Podrška za razne platforme: preglednici, Word, Google Docs.  
+- Personalizirani savjeti za poboljšanje pisanja.
+
+---
+
+# Nedostaci Grammarlyja
+
+**Ograničenja alata:**  
+- Premium verzija nudi ključne funkcionalnosti, dok besplatna ima ograničenja.  
+- Povremeno neprilagođeni prijedlozi za tehnički sadržaj.  
+- Zahtijeva internetsku vezu za rad.  
+
+*Tablica prednosti i nedostataka:*
+
+| **Prednosti**              | **Nedostaci**                     |
+|-----------------------------|------------------------------------|
+| Jednostavno za korištenje  | Ograničena besplatna verzija       |
+| Integracija s popularnim alatima | Potreban internet za rad        |
+
+---
+
+# Zaključak
+
+**Osobni osvrt:**  
+Grammarly je koristan alat za svakodnevno i profesionalno pisanje. Korištenje alata mi je pomoglo u izradi kvalitetnijih tekstova s manje pogrešaka.
+
+**Preporuke:**  
+- Idealno za studente, IT stručnjake i sve koji redovito pišu formalne tekstove.
+
+---
+
+# Reference
+
+1. [Službena stranica Grammarly](https://www.grammarly.com)  
+2. Članak: "Kako Grammarly poboljšava pisanje" - Autor: Ime Prezime  
+3. [Vodič za početnike](https://example.com/guide)  
+4. Video vodič: "Grammarly - Alat za početnike"  
+
+---
+
+# Hvala na pažnji!
+
+**Pitanja?**  
+Slobodno pitajte ako imate dodatnih pitanja o alatu Grammarly!
+
+---
